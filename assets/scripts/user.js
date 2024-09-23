7f57d6801d05a00f1bbc995aa419bb73f6e05130
GuilhermeRossiKirsten <guirossik10@gmail.com>
2024-09-23 19:02:37 -0300

    fix time in gerente.html

--- assets/scripts/user.js
+++ assets/scripts/user.js
@@ -1,11 +1,22 @@
-
-
-
 const user = JSON.parse(localStorage.getItem("currentUser"));
 
 const nome = user.name.split(" ");
 
 const elements = document.querySelectorAll(".user1");
-elements.forEach(element => {
-    element.innerText = nome[0]
+elements.forEach((element) => {
+  element.innerText = nome[0];
 });
+
+const dataAtual = new Date();
+
+let periodoDoDia;
+const hora = dataAtual.getHours();
+if (hora >= 6 && hora < 12) {
+  periodoDoDia = "Bom dia";
+} else if (hora >= 12 && hora < 18) {
+  periodoDoDia = "Boa tarde";
+} else {
+  periodoDoDia = "Boa noite";
+}
+
+document.getElementById("tempo").innerText = periodoDoDia;
